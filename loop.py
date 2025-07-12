0563c8c3719274e7dbeb85d1a07c917363595417
oneoriginal <one.chung.2013@gmail.com>
2025-07-12 05:54:56 +0000

    20250712

--- loop.py
+++ loop.py
@@ -124,25 +124,80 @@
 
 # Sample of using infinite loop, continue, break
 
-while(True) :
-    print("Please choose a menu option!")
-    print("1. Print hello")
-    print("2. Print hi")
-    print("3. Exit")
-    menu = int(input())
+# while(True) :
+#     print("Please choose a menu option!")
+#     print("1. Print hello")
+#     print("2. Print hi")
+#     print("3. Exit")
+#     menu = int(input())
 
-    if(menu == 1) :
-        print("hello")
-    elif(menu == 2) :
-        print("hi")
-    elif(menu == 3) :
-        print("Exiting the menu program.")
-        break
-    else :
-        print("Invalid input. Please enter a number between 1 and 3.")
-        print()
-        print()
-        continue
+#     if(menu == 1) :
+#         print("hello")
+#     elif(menu == 2) :
+#         print("hi")
+#     elif(menu == 3) :
+#         print("Exiting the menu program.")
+#         break
+#     else :
+#         print("Invalid input. Please enter a number between 1 and 3.")
+#         print()
+#         print()
+#         continue
 
-    print()
-    print()
+#     print()
+#     print()
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# Nested loops
+# A loop inside another loop
+# Two nested loops are called double nested, three nested loops are called triple nested.
+# More than triple nested loops are rarely used.
+
+# for x in range(1, 10, 1) :                  # repeats 9 times
+#     for y in range(1, 10, 1) :              # repeats 81 times in total
+#         print(x, "*", y, "=", x * y)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# # Create a 1 - 19 multiplication table (needs to end with 19*9)
+# for x in range(1, 20, 1) :
+#     for y in range(1, 10, 1) :
+#         print(x, "*", y, "=", x * y)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# while() multiplication table
+# x = 1
+# while(x < 10) :                           # This produces the same result as the for command above
+#     y = 1
+#     while(y < 10) :
+#         print(x, "*", y, "=", x * y)
+#         y += 1
+#     x += 1
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# for x in range(1, 10, 1) :
+#     for y in range(1, x+1, 1) :             # Can use an external variable in range
+#         print(x, "*", y, "=", x * y)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# for i in range(1, 9, 1) :                        # This is 1 - 8
+#     for j in range(i, i + 3, 1) :                # This is (example), i (= 1), i + 3 (4), by 1, so 1, 2, 3
+#         print(j)                                 # The code must end with the number 10.
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# In the end, there must be five Bs.
+
+# for i in range(1, 6, 1) :
+#     print('A')
+#     for j in range(0, i, 1):
+#         print('B')
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
